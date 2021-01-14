dccf0f3575dd9d64940798496d17859f3e2dbc07
tecnodesignc <marcos21.009@gmail.com>
2021-01-13 22:19:43 -0500

    Inital Commit

---
+++ vendor/composer/installers/src/Composer/Installers/EncoreInstaller.php
@@ -0,0 +1,49 @@
+<?php
+namespace Composer\Installers;
+
+class EncoreInstaller extends BaseInstaller
+{
+    protected $locations = array(
+        'module' => 'Modules/{$name}/',
+        'theme' => 'Themes/{$name}/'
+    );
+
+    /**
+     * Format package name.
+     *
+     * For package type encore-module, cut off a trailing '-plugin' if present.
+     *
+     * For package type encore-theme, cut off a trailing '-theme' if present.
+     *
+     */
+    public function inflectPackageVars($vars)
+    {
+        if ($vars['type'] === 'encore-module') {
+            return $this->inflectPluginVars($vars);
+        }
+
+        if ($vars['type'] === 'encore-theme') {
+            return $this->inflectThemeVars($vars);
+        }
+
+        return $vars;
+    }
+
+    protected function inflectPluginVars($vars)
+    {
+        $vars['name'] = preg_replace('/-module$/', '', $vars['name']);
+        $vars['name'] = str_replace(array('-', '_'), ' ', $vars['name']);
+        $vars['name'] = str_replace(' ', '', ucwords($vars['name']));
+
+        return $vars;
+    }
+
+    protected function inflectThemeVars($vars)
+    {
+        $vars['name'] = preg_replace('/-theme$/', '', $vars['name']);
+        $vars['name'] = str_replace(array('-', '_'), ' ', $vars['name']);
+        $vars['name'] = str_replace(' ', '', ucwords($vars['name']));
+
+        return $vars;
+    }
+}
